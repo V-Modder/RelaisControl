460f604c3a8cd58d3e05ad657f47399d2111ab8b
V-Modder <V-Modder@web.de>
2018-01-05 22:38:22 +0100

    Removed unwanted return

--- main.c
+++ main.c
@@ -44,8 +44,7 @@ int main(int argc, char **argv)
 	if(inputParam == 0) {
 		return 0;
 	}
-	printf("%d \n", inputParam);
-	return 0;
+	// printf("%d \n", inputParam);
 	
 	
 	for(i = 0; i < MAX_DEVICES; i++) {
